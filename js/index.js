16c043acdc2cb9d7d017958678ea72cdb60a06c6
riya-varghese <57456903+riya-varghese@users.noreply.github.com>
2021-01-18 23:11:38 -0500

    alert

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-const player = document.getElementById('player');
+/*const player = document.getElementById('player');
 const history = document.getElementById('history');
 
 function jumplava(){
@@ -11,3 +11,33 @@ function jumplava(){
 document.addEventListener('keyup', () => {
     jumplava();
 });
+*/
+//jumpfunction
+const player = document.getElementById('player');
+const history = document.getElementById('history');
+console.log(player);
+function jumplava(){
+    player.classList.add("jump-over");
+    setTimeout(() => {
+        player.classList.remove('jump-over');
+    }, 600);
+};
+
+document.addEventListener('keypress', event => {
+    jumplava();
+});
+
+//fail
+const seeIfFail = setInterval(function(){
+    const playerRight =
+    parse(window.getComputedStyle(player).
+    getPropertyValue("right"));
+    const LavaRightt =
+    parse(window.getComputedStyle(lava).
+    getPropertyValue("right"));
+    if(lavaRightt=173 && lavaRightt=173 && playerRight=70)
+        {
+            alert("fail")
+        }
+}, 5)
+//input
